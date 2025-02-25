dd3ebfffb959ff6aa0c3ffcf8a736807af5d67e1
Karissa Wiggins <126825648+kwigs0804@users.noreply.github.com>
2025-02-24 19:15:08 -0500

    added main method

---
+++ ComputerSecurity(CS492)/BlockCipher.java
@@ -0,0 +1,10 @@
+//Karissa Wiggins
+import java.util.*;
+
+public class BlockCipher {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter plaintext (in hex): ");
+        String plain=scan.nextLine();
+    }
+}
