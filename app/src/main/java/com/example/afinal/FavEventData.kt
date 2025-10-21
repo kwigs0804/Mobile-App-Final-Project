84c0bfc6c00c09d3ce8a1e562c81f4009fc682a1
Karissa Wiggins <126825648+kwigs0804@users.noreply.github.com>
2025-10-21 17:10:53 -0400

    made FavEventData no-arg constructor as required by Firestone toObject(), fixed crashes

--- app/src/main/java/com/example/afinal/FavEventData.kt
+++ app/src/main/java/com/example/afinal/FavEventData.kt
@@ -1,12 +1,12 @@
 package com.example.afinal
 
 data class FavEventData(
-    val id:String,
-    val name:String,
-    val date: String,
-    val venue: String,
-    val address: String,
-    val city: String,
-    val imagesUrl: String,
+    val id:String="",
+    val name:String="",
+    val date: String="",
+    val venue: String="",
+    val address: String="",
+    val city: String="",
+    val imagesUrl: String="",
     var favorite: Boolean=true
 )
